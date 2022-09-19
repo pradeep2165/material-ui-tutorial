2362c07f2e740a9d722d61e1eecad59631df6660
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-19 22:00:01 +0530

    styled added

--- src/component/MuiButton.tsx
+++ src/component/MuiButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Button, Stack} from '@mui/material'
 function MuiButton() {
   return (
-    <Stack>
+    <Stack spacing={2} direction='row'>
       <Button variant="text">Text</Button>
       <Button variant="contained">contained</Button>
       <Button variant="outlined">outlined</Button>
