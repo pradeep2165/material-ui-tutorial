440ab2b395fc980b6a928a191c800e6f65c96568
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-19 20:58:05 +0530

    heading tag

---
+++ src/component/MuiTypography.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+const MuiTypography=()=> {
+  return (
+    <>
+    
+    <Typography variant='h1'>
+    h1 heading     
+    </Typography>
+
+    <Typography variant='h2'>
+    h2 heading
+    </Typography>
+
+    <Typography variant='h3'>
+    h3 heading
+    </Typography>
+
+    <Typography variant='h4'>    
+    h4 heading
+    </Typography>
+    
+
+    <Typography variant='h5'>
+    h5 heading
+    </Typography>
+    
+
+    <Typography variant='h6'>
+    h6 heading
+    </Typography>
+
+    
+
+
+    </>
+  )
+}
+
+export default MuiTypography
